e7ce986ccaaa81354c3ad42e96bd56da7d798555
Andy <andreas.clausen@hotmail.com>
2024-08-30 16:54:50 +0200

    feat: styling and layout

--- src/components/KanBan/KanBanBoard.vue
+++ src/components/KanBan/KanBanBoard.vue
@@ -1,15 +1,21 @@
 <template>
   <v-container>
-    <v-card class="mx-auto" max-width="100%">
-      <v-card-title>Kanban Board</v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col v-for="column in columns" :key="column.id" cols="auto">
-              <KanBanColumn :column="column" />
-            </v-col>
-          </v-row>
-        </v-container>
+    <v-card :color="color" height="80dvh" class="d-flex flex-column">
+      <v-card-title class="font-weight-bold">Kanban Board</v-card-title>
+      <v-card-text class="flex-grow-1 pb-0">
+        <v-row class="overflow-x-scroll flex-nowrap fill-height">
+          <v-col v-for="column in columns" :key="column.id" cols="auto">
+            <KanBanColumn :column="column" />
+          </v-col>
+          <v-col cols="auto">
+            <v-container
+              class="h-50 border-lg rounded-lg border-success d-flex justify-center align-center cursor-pointer"
+              width="240px"
+            >
+              <span class="text-success font-weight-bold text-h2">+</span>
+            </v-container>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-container>
@@ -20,4 +26,6 @@ import { useAppStore } from "@/stores/app";
 
 const store = useAppStore();
 const columns = store.columns;
+
+const color = "#F2B8B4";
 </script>
--- src/components/KanBan/KanBanColumn.vue
+++ src/components/KanBan/KanBanColumn.vue
@@ -1,11 +1,22 @@
 <template>
-  <v-card class="mx-auto" max-width="344">
+  <v-card :color="color" max-width="344">
     <v-card-title>{{ column.title }}</v-card-title>
     <v-card-text>
       <v-container dropzone="move">
         <v-row v-for="card in column.cards" :key="card.id">
           <v-col cols="12">
             <KanBanCard :card="card" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-container
+              class="border-lg rounded-lg border-success d-flex justify-center align-center cursor-pointer"
+              width="100%"
+              height="80px"
+            >
+              <span class="text-success font-weight-bold text-h2">+</span>
+            </v-container>
           </v-col>
         </v-row>
       </v-container>
@@ -17,4 +28,6 @@
 import type { Column } from "@/stores/app";
 
 const { column } = defineProps<{ column: Column }>();
+
+const color = "#FFECBE";
 </script>
